0e18ab0fb704355adb73ac277072a572c05eadba
Asmaa <Asmaa@DESKTOP-S91DDCE>
2025-09-09 07:41:03 +0300

    Your commit message here

---
+++ 3.OOP/OOP_Route/Assignment Three/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_Three
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
