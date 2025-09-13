a526d8fc7e5b73f10e558973a289a68cd09c2add
Asmaa <Asmaa@DESKTOP-S91DDCE>
2025-09-13 03:52:41 +0300

    Your commit message here

--- 7.MVC Core/MVC_Route/Test/Controllers/MoviesController.cs
+++ 7.MVC Core/MVC_Route/Test/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Test.Models;
 
 namespace Test.Controllers
 {
@@ -10,9 +11,40 @@ namespace Test.Controllers
         }
 
         // BaseUrl/Movies/GetMovie/10
-        public void GetMovie(int? id)
+        [HttpGet] // I Return Data From Database
+        public IActionResult GetMovie(int? id)
         {
+            //ContentResult result=new ContentResult();
+            //result.Content = $"Movie With Id : {id} Is Avatar";
+            //return result;
+            // return $"Movie With Id : {id} Is Avatar";
 
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            else if (id > 100)
+            {
+                return NotFound();
+            }
+            else if (id > 0 && id < 100)
+            {
+                return Content($"Movie With Id : {id} Is Avatar");
+            }
+            else 
+                return Content("An Error");
         }
+
+        public IActionResult TestRedirectAction()
+        {
+            return Redirect("https://localhost:44302/Movies/GetMovie/25");
+        }
+
+        [HttpPost] // I Take Input To Put It In Database
+        public IActionResult TestModelBinding([FromBody]Movie movie)
+        {
+            return Content($"Movie With Id : {movie.Id} The Film is :: {movie.Title}");
+        }
+
     }
 }
--- 7.MVC Core/MVC_Route/Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ 7.MVC Core/MVC_Route/Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e18ab0fb704355adb73ac277072a572c05eadba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf152bfc1a7122e2d86e87948969a171d15fc1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
